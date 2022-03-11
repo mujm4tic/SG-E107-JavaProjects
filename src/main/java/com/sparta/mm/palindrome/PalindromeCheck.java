23d37ed0813925c0838d0720e1a13dd34551af1c
mujm4tic <mmoosavi@hotmail.co.uk>
2022-03-11 02:39:11 +0000

    palindrome word check and longest palindrome in sentence projects added

---
+++ src/main/java/com/sparta/mm/palindrome/PalindromeCheck.java
@@ -0,0 +1,16 @@
+package com.sparta.mm.palindrome;
+
+public class PalindromeCheck {
+    public static boolean isPalindrome(String wordCheck){
+        String reverse = "";
+        int length = wordCheck.length();
+        for(int i = length -1; i>=0; i-- ){ //review logic
+            reverse = reverse + wordCheck.charAt(i);
+        }   return wordCheck.equalsIgnoreCase(reverse);
+    }
+
+    public static void main(String[] args) {
+        String newWord = new String("madam");
+        System.out.println(isPalindrome(newWord));
+    }
+}
